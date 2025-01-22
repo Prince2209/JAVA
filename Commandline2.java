ab2eeb6c4a3305e442e76c5c27cebd0aca5b028c
Your Name <princekardani2006@gmail.com>
2025-01-22 11:18:56 +0530

    1

---
+++ Commandline2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Commandline2{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+			
+			// nextLine()											
+		// System.out.println("Enter String 1: ");
+		// String a=sc.nextLine();
+		// System.out.println(a);
+
+			// next()
+		// System.out.println("Enter String 2: ");
+		// String b=sc.next();
+		// System.out.println(b);
+
+			// integer
+		// System.out.println("Enter String 3: ");
+		// int c=sc.nextInt();
+		// System.out.println(c);
+
+			// double
+		System.out.println("Enter String 4: ");
+		double d=sc.nextDouble();
+		System.out.println(d);
+	}
+}
